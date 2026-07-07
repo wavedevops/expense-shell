e36b268f002d9e7a9a77cf6de215f4bd0c2a2d3f
Chowdary-Hari <chandrahari321@outlook.com>
2026-07-07 18:27:54 +0530

    automated commit #75

---
+++ frontend.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+head_name() {
+    echo -e "\e[33m>>>>>>>>>> $* <<<<<<<<<<\e[0m"
+}
+status_check() {
+    echo "exit status $?"
+}
+
+
+
+head_name install nginx
+dnf install nginx -y 
+status_check
+
+head_name remove the old web contenrt
+rm -rf /usr/share/nginx/html/* 
+status_check
+
+head_name Download the frontend content
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip 
+status_check
+
+head_name Extract the frontend content
+unzip -o /tmp/frontend.zip -d /usr/share/nginx/html
+status_check
+# -o  override , -d destination
+
+head_name copy expence conf file
+cp -r expense.conf /etc/nginx/default.d/expense.conf
+status_check
+
+head_name enable nginx service
+systemctl enable nginx
+status_check
+
+head_name restart nginx service
+systemctl restart nginx
+status_check
