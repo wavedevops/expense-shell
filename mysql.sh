16c29d908d4bf082c44ef9a7a4c26fb15d7090f1
Chowdary-Hari <chandrahari321@outlook.com>
2026-07-07 18:59:15 +0530

    automated commit #80

---
+++ mysql.sh
@@ -0,0 +1,27 @@
+head_name() {
+    echo -e "\e[33m>>>>>>>>>> $* <<<<<<<<<<\e[0m"
+}
+status_check() {
+    echo "exit status $?"
+}
+
+head_name "Setup MySQL Root Password"
+export MYSQL_ROOT_PASSWORD="ExpenseApp@1"
+status_check
+
+head_name install Mysql
+dnf install mysql-server -y
+status_check
+
+head_name enable and start Mysqkl server
+systemctl enable mysqld
+systemctl start mysqld  
+status_check
+
+head_name setup mysql root password 
+mysql_secure_installation --set-root-pass $MYSQL_ROOT_PASSWORD
+status_check
+
+
+# mysqld → Database server
+# mysql → Client 
